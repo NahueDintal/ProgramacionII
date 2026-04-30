97cf2a2903e6219c74e747623a1fd087b628f1ec
NaCk3rZ! <nahue.dintal@gmail.com>
2026-04-30 10:14:42 -0300

    empezamos con el primer ejercicio de clases, agregado de pedidos

--- GestionPedidosOnLine/src/Main.java
+++ GestionPedidosOnLine/src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("¡Hola Wachin! Desde 'GestionPedidosOnLine' con Java 17!");
-    }
+  public static void main(String[] args) {
+  }
 }
